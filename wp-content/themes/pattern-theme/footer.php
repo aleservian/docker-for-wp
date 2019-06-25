a525f1694c014d460ad80f2d9818fc105b06a222
Alexandre Servian <alexandreservian@gmail.com>
2019-06-25 09:49:44 -0300

    ultimo commit antes do backup

--- wp-content/themes/pattern-theme/footer.php
+++ wp-content/themes/pattern-theme/footer.php
@@ -1,3 +1,8 @@
-
+  <script src="<?php bloginfo('template_url')?>/assets/js/basket.min.js"></script>
+  <script type="text/javascript">
+    basket.clear(true);
+    //basket.clear();
+    basket.require({ url: '<?php bloginfo('template_url')?>/assets/js/main.js',unique: 'v1'});
+  </script>
   </body>
 </html>
